dce1d7db51906f61f408b1a04fe5e717848567cf
msteffen <matthew.steffen@gmail.com>
2015-08-18 02:09:14 -0700

    Change flag descriptions

--- backup.py
+++ backup.py
@@ -16,21 +16,20 @@ def main():
     ./backup.py --src=/home/mjs/sensitive --dst=/net/backups/today --prev_backup=/net/backups/6_months_ago
       """,
       formatter_class=argparse.RawTextHelpFormatter)
-
   arg_parser.add_argument('--src', type=str, help="The directory to be backed "
       "up")
-
   # Either --backup_drive or --dst/--prev_backup should be specified
   arg_parser.add_argument('--backup_drive', type=str, help="A directory where "
-      "the backup should be placed, which follows this script's naming "
-      "convention. --dst will be constructed and --prev_backup will be "
-      "inferred if this flag is set.")
+      "the new backup should be placed, which follows this script's naming "
+      "convention for incremental backups (backups are named with the date "
+      "they were taken). --dst will be constructed and --prev_backup will be "
+      "inferred if this flag is set. If this is set, don't set those flags")
   arg_parser.add_argument('--dst', type=str, help="The directory that will "
       "receive the backup")
   arg_parser.add_argument('--prev_backup', type=str, help="A directory used "
-      "for a previous backup. To save space, files that haven't changed since "
-      "this backup was taken are hardlinked here from --dst, instead of "
-      "copied")
+      "for a previous backup. To save space, if a file hasn't changed since "
+      "this backup was taken, the file is backed up by creating a hard link to "
+      "here from --dst, instead of being copied")
   args = arg_parser.parse_args()
 
   # Validate flag values
@@ -50,8 +49,8 @@ def main():
     backup = Backup.FromBackupDrive(args.backup_drive)
   else:
     backup = Backup(args.src, args.dst, args.prev_backup)
+  
   backup.backup_with_rsync()
-
   print("\033[1;32mDONE!\033[0m")
 
 class Backup:
